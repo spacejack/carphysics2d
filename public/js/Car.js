be10a2266222caf2dab67953dd2b745907fd40fb
Fasand <peter@petermanas.com>
2019-10-19 14:30:06 +0100

    change Car size defaults

--- public/js/Car.js
+++ public/js/Car.js
@@ -66,20 +66,20 @@ Car.Config = function( opts )
 	opts = opts || {};
 	//  Defaults approximate a lightweight sports-sedan.
 	this.gravity = opts.gravity || 9.81;  // m/s^2
-	this.mass = opts.mass || 1200.0;  // kg
+	this.mass = opts.mass || 900.0;  // kg
 	this.inertiaScale = opts.inertiaScale || 1.0;  // Multiply by mass for inertia
-	this.halfWidth = opts.halfWidth || 0.8; // Centre to side of chassis (metres)
-	this.cgToFront = opts.cgToFront || 2.0; // Centre of gravity to front of chassis (metres)
-	this.cgToRear = opts.cgToRear || 2.0;   // Centre of gravity to rear of chassis
-	this.cgToFrontAxle = opts.cgToFrontAxle || 1.25;  // Centre gravity to front axle
-	this.cgToRearAxle = opts.cgToRearAxle || 1.25;  // Centre gravity to rear axle
-	this.cgHeight = opts.cgHeight || 0.55;  // Centre gravity height
+	this.halfWidth = opts.halfWidth || 0.4; // Centre to side of chassis (metres)
+	this.cgToFront = opts.cgToFront || 1.0; // Centre of gravity to front of chassis (metres)
+	this.cgToRear = opts.cgToRear || 1.0;   // Centre of gravity to rear of chassis
+	this.cgToFrontAxle = opts.cgToFrontAxle || 0.625;  // Centre gravity to front axle
+	this.cgToRearAxle = opts.cgToRearAxle || 0.625;  // Centre gravity to rear axle
+	this.cgHeight = opts.cgHeight || 0.275;  // Centre gravity height
 	this.wheelRadius = opts.wheelRadius || 0.3;  // Includes tire (also represents height of axle)
 	this.wheelWidth = opts.wheelWidth || 0.2;  // Used for render only
 	this.tireGrip = opts.tireGrip || 2.0;  // How much grip tires have
 	this.lockGrip = (typeof opts.lockGrip === 'number') ? GMath.clamp(opts.lockGrip, 0.01, 1.0) : 0.7;  // % of grip available when wheel is locked
-	this.engineForce = opts.engineForce || 8000.0;
-	this.brakeForce = opts.brakeForce || 12000.0;
+	this.engineForce = opts.engineForce || 4000.0;
+	this.brakeForce = opts.brakeForce || 6000.0;
 	this.eBrakeForce = opts.eBrakeForce || this.brakeForce / 2.5;
 	this.weightTransfer = (typeof opts.weightTransfer === 'number') ? opts.weightTransfer : 0.2;  // How much weight is transferred during acceleration/braking
 	this.maxSteer = opts.maxSteer || 0.6;  // Maximum steering angle in radians
